50d7488bc628cda88651bd3f46290b38504d39f4
Faizan Ali Baig <142169392+faizanalibaig@users.noreply.github.com>
2024-03-24 16:02:42 +0500

    Update main.cpp

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int SelectionSort(int array[], int size){
      for(int i=0; i<size-1; i++){
        for(int j=i+1; j<size; j++){
-          if(array[i]>array[j]){
+          if(array[i]>array[j]){ //
            int temp=array[j];
            array[j]=array[i];
            array[i]=temp;
