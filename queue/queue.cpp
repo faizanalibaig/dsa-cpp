9b1c3e141661e9fda87f18b81b24b8b8592a7996
Faizan Ali Baig <142169392+faizanalibaig@users.noreply.github.com>
2024-05-04 23:04:27 +0500

    queue

---
+++ queue/queue.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#define size 100
+using namespace std;
+
+class queue {
+    int *arr;
+    int front;
+    int rear;
+
+public:
+    queue() {
+        arr = new int[size];
+        front = 0;
+        rear = 0;
+    }
+
+    void push(int data) {
+        if (rear == size) {
+            cout << "queue overflow" << endl;
+            return;
+        }
+
+        arr[rear] = data;
+        rear++;
+    }
+
+    void pop() {
+        if (front == rear) {
+            cout << "queue underflow" << endl;
+            return;
+        }
+        arr[front]=-1;
+        front++;
+    }
+
+    bool empty() {
+        return front == rear;
+    }
+
+    int getfront() {
+        if (front == rear) {
+            cout << "queue underflow" << endl;
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    int getRear() {
+        if (front == rear) {
+            cout << "queue underflow" << endl;
+            return -1;
+        }
+
+        return arr[rear - 1];
+    }
+
+   void peek() {
+    if (front == rear) {
+        cout << "queue underflow" << endl;
+        return;
+    }
+
+    for (int i = front; i < rear; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+};
+
+int main() {
+    queue q;
+    q.push(1);
+    q.push(2);
+
+    q.peek();
+
+    return 0;
+}
